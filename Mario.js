97471d76f3f8993c12ce8907f59f706aa7fec522
wjddbwls37 <wjddbwls37@naver.com>
2023-06-13 11:04:06 +0900

    zino에게 맡기기 직전

--- Mario.js
+++ Mario.js
@@ -1,3 +1,9 @@
 class Mario extends GameObject{
 
+    
+    constructor(container, src, width, height, x, y, velX,velY){
+
+        super(container, src, width, height, x, y, velX,velY);
+    };
+
 }
